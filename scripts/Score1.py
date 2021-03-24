0b88e994a45f3fbe66dadfa1a5ffd539ff86c222
F0LCR0M <50242710+F0LCR0M@users.noreply.github.com>
2021-03-24 12:19:36 +0100

    updated the scoring function with demo functionality

--- scripts/Score1.py
+++ scripts/Score1.py
@@ -14,65 +14,52 @@ Score 1 node
 
 import rospy
 from std_msgs.msg import String
-from Strategy.srv import ListRequest, Info
+# from Strategy.srv import ListRequest, Info
+import json
 
-class Score1:
     
-    def __init__ (self):
-        pass
-    
-    
-    def calculate_score1(slef, deploy_time, n_density, info_desnity):
-        
-        score_trophy = []
-        
-        for trophy in trophy_list
-        
-            score = alpha1*info_gain + alpha2*deploy_time + alpha3*n_density
-            score_trophy.append((score, trophy))
-            
-        return score_trophy = []
-    
-    def calculate_info_density(self, get_info):
-        
-        # use service get_info to calculate info_density
-        
-        return info_density
-    
-    def calculate_deploy_time(self, get_info):
-        
-        # Use standar path file to calculate deply time
-        
-        return deploy_time
-        
-    def calculate_n_density(self, get_list):
-        
-        #use trophy list to calculate n_density
-        
-        return n_density
+def calculate_score1(trophy_list, info):
+    max_val = (0, None)
+    for trophy in trophy_list:
+        pos = trophy["pos"]
+        deploy_time = info["deploy"][trophy]
+        n_density = calculate_n_density(trophy, info)
+        info_gain = info["info_gain"][trophy]
+        score = 1.0 * deploy_time + 0 * n_density + 0 * info_gain
+        if score > max_val[0]:
+            max_val = (score, trophy)
+
+    return max_val[1]
+
+
+def calculate_n_density(trophy, info):
+    return 0.5
+
 
 def main():
     
-    rospy.init_node(‘Score1’, anonymous=True)
+    rospy.init_node("Score1", anonymous=True)
     
-    get_list = rospy.ServiceProxy('get_list', ListRequest )
-    get_info = rospy.ServiceProxy('info', Info )
+    # get_list = rospy.ServiceProxy('get_list', ListRequest)
+    # get_info = rospy.ServiceProxy('info', Info)
     
-    n_density = calculate_n_density(get_list)
-    deploy_time = calculate_deploy_time(get_info)
-    info_desnity = calculate_info_density(get_info)
+    # goal_trophy = calculate_score1(get_list, get_info)
+    # convert into message
+
+    message = {
+        "id": "131",
+        "description": {
+            "x": 20,
+            "y": 30,
+            "alpha": -90,
+            "z": 40
+        }
+    }
     
-    score_trophy = calculate_score1(n_density, deploy_time, info_density)
-    
-    #Find trophy with maximum score
-    
-    max_score = numpy.where(score_trophy == numpy.amax(score_trophy))
-    
-    pub = rospy.Publisher('\movement', String, queue_size=2)
-    pub.publish(max_score)
-    rate = rospy.Rate(10)
+    pub = rospy.Publisher('/target_move', String, queue_size=2)
+    msg = json.dumps(message)
+    pub.publish(msg)
     
 
-        
 if __name__ == '__main__':
     main()
