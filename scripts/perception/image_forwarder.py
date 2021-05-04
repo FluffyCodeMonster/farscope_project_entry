54257902b5770f8fc330135815f33190385adc6d
Freddie Turner <jx20025@bristol.ac.uk>
2021-05-04 17:34:48 +0100

    Tidied up image_forwarder.py.

--- scripts/perception/image_forwarder.py
+++ scripts/perception/image_forwarder.py
@@ -8,13 +8,6 @@ from sensor_msgs.msg import Image
 # Use an empty message to send a signal.
 from std_msgs.msg import Empty
 
-#def on_image(image):
-#    if ((rospy.get_rostime()).nsecs - last_time_nsecs > pub_intv_nsecs):
-#        last_time_nsecs = (rospy.get_rostime()).nsecs
-#        image_pub.publish(image)
-
-#pub_intv_nsecs = 1000000000 # Publish image every 4 seconds.
-#last_time_nsecs = (rospy.get_rostime()).nsecs
 
 # TODO Will these two block each other? Do I need multithreading? Could multithreading lead
 # to a race condition s.t. a request is missed?
@@ -22,9 +15,6 @@ from std_msgs.msg import Empty
 class RequestListener (threading.Thread):
     def __init__(self, threadID, name, counter):
         threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
-        self.counter = counter
     
     def run(self):
         print('Starting request listener thread')
@@ -32,9 +22,6 @@ class RequestListener (threading.Thread):
 
 def on_image(image):
     global image_requested
-    #if image_requested:
-    #    image_pub.publish(image)
-    #    print('Success')
     # If a new image has been requested:
     if image_requested:
         image_pub.publish(image)
@@ -53,7 +40,7 @@ image_sub = rospy.Subscriber("camera1/image_raw", Image, on_image, queue_size=1)
 image_pub = rospy.Publisher("image_for_trophy_detection", Image)
 
 # Start request listener thread:
-request_listener = RequestListener(1, "Request listener", 1)
+request_listener = RequestListener()
 request_listener.start()
 
 # Keep alive
